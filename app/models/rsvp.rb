0c30de1a38749f839cc0ca205fda6715e33d7c63
Evan Tedesco <Evantedesco@gmail.com>
2014-05-04 17:02:45 -0600

    validates presence of required fields

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,3 +1,5 @@
 class Rsvp < ActiveRecord::Base
-
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :attending, presence: true
 end
--- spec/features/rsvps_spec.rb
+++ spec/features/rsvps_spec.rb
@@ -10,4 +10,15 @@ feature 'Rsvp manager' do
     click_button 'submit'
     expect(page).to have_content'Login'
   end
+
+  scenario 'It renders an error if required feilds are blank'do
+    visit '/'
+    click_on 'RSVP'
+    fill_in '*Full Name', with: ''
+    fill_in '*Email', with: ''
+    click_button 'submit'
+    expect(page).to have_content "Name can't be blank"
+    expect(page).to have_content "Email can't be blank"
+    expect(page).to have_content "Attending can't be blank"
+  end
 end
