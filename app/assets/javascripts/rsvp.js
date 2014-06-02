80567c9295c8950e8a6a95b7124630ff8ad64943
Evan Tedesco <Evantedesco@gmail.com>
2014-06-01 18:36:16 -0600

    fixed user meal choice JS

--- app/assets/javascripts/rsvp.js
+++ app/assets/javascripts/rsvp.js
@@ -1,22 +1,32 @@
 var hideGuestsAndSetupEvents = function hideGuestsAndSetupEvents() {
     if ($('#rsvp-container').length > 0) {
         var numberOfGuests = $('.number-of-guests'),
+            userMeal = $('.user-meal-choice'),
             accept = $('#rsvp_attending_true'),
             decline = $('#rsvp_attending_false');
 
         numberOfGuests.hide()
+        userMeal.hide()
         accept.change(function () {
             numberOfGuests.show({
                 duration: 400,
                 easing: 'linear'
-            })
+            }),
+                userMeal.show({
+                    duration: 400,
+                    easing: 'linear'
+                })
         });
 
         decline.change(function () {
             numberOfGuests.hide({
                 duration: 400,
                 easing: 'linear'
-            })
+            }),
+                userMeal.hide({
+                    duration: 400,
+                    easing: 'linear'
+                })
         });
 
 
