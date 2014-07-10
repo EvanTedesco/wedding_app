ff97037a37a1664dda15ffebaa272910091edeb1
Evan Tedesco <Evantedesco@gmail.com>
2014-07-10 11:13:55 -0600

    Normalizing test styling

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,27 +1,28 @@
 require 'spec_helper'
 
 describe UserMailer do
-before do
-   DatabaseCleaner.clean
-   @user = create_user
+  before do
+    DatabaseCleaner.clean
+    @user = create_user
+  end
 
-end
-
-describe 'message when user is created' do
-
-   let(:message) { UserMailer.welcome_email(@user) }
+  describe 'message when user is created' do
+    let(:message) { UserMailer.welcome_email(@user) }
 
 
-   it 'comes from the proper user' do
-     expect(message.from).to eq ['evan@example.com']
-   end
+    it 'comes from the proper user' do
 
-   it 'goes to the proper user' do
-     expect(message.to).to eq ['bob@example.com']
-   end
+      expect(message.from).to eq ['evan@example.com']
+    end
 
-   it 'has the proper subject' do
-     expect(message.subject).to eq 'Let\'s party!'
-   end
-end
+    it 'goes to the proper user' do
+
+      expect(message.to).to eq ['bob@example.com']
+    end
+
+    it 'has the proper subject' do
+      
+      expect(message.subject).to eq 'Let\'s party!'
+    end
+  end
 end
