574b4fdda6b395155f4194a3be7287c927de50c5
Evan Tedesco <Evantedesco@gmail.com>
2014-07-09 21:17:59 -0600

    Added validation for capitalization of email for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,27 +5,38 @@ describe User do
     DatabaseCleaner.clean
   end
 
-it 'Prevents duplicate emails from being created' do
-   valid_user = User.create!(email: 'evan@example.com', password: 'password', name:'Evan Tedesco', max_guests:1, admin: true)
-   invalid_user = User.create(email: 'evan@example.com', password: 'password', name:'Evan Tedesco', max_guests:1, admin: true)
+  context 'validations' do
 
-   expect(valid_user).to be_valid
-   expect(invalid_user).to be_invalid
-end
+    it 'Prevents duplicate emails from being created' do
+      valid_user = User.create!(email: 'evan@example.com', password: 'password', name: 'Evan Tedesco', max_guests: 1, admin: true)
+      invalid_user = User.create(email: 'evan@example.com', password: 'password', name: 'Evan Tedesco', max_guests: 1, admin: true)
 
-it 'Prevents users from being created without an email' do
-   valid_user = User.create!(email: 'evan@example.com', password: 'password', name:'Evan Tedesco', max_guests:1, admin: true)
-   invalid_user = User.create(email: '', password: 'password', name:'Evan Tedesco', max_guests:1, admin: true)
+      expect(valid_user).to be_valid
+      expect(invalid_user).to be_invalid
+    end
 
-   expect(valid_user).to be_valid
-   expect(invalid_user).to be_invalid
-end
+    it 'Prevents users from being created without an email' do
+      valid_user = User.create!(email: 'evan@example.com', password: 'password', name: 'Evan Tedesco', max_guests: 1, admin: true)
+      invalid_user = User.create(email: '', password: 'password', name: 'Evan Tedesco', max_guests: 1, admin: true)
 
-it 'Prevents users from being created with invalid guest count' do
-   valid_user = User.create!(email: 'evan@example.com', password: 'password', name:'Evan Tedesco', max_guests:1, admin: true)
-   invalid_user = User.create(email: 'evan@example.com', password: 'password', name:'Evan Tedesco', max_guests:'abc', admin: true)
+      expect(valid_user).to be_valid
+      expect(invalid_user).to be_invalid
+    end
 
-   expect(valid_user).to be_valid
-   expect(invalid_user).to be_invalid
-end
+    it 'Prevents users from being created with invalid guest count' do
+      valid_user = User.create!(email: 'evan@example.com', password: 'password', name: 'Evan Tedesco', max_guests: 1, admin: true)
+      invalid_user = User.create(email: 'evan@example.com', password: 'password', name: 'Evan Tedesco', max_guests: 'abc', admin: true)
+
+      expect(valid_user).to be_valid
+      expect(invalid_user).to be_invalid
+    end
+
+    it 'Ignores capitalization in email' do
+      valid_user = User.create!(email: 'evan@example.com', password: 'password', name: 'Evan Tedesco', max_guests: 1, admin: true)
+      invalid_user = User.create(email: 'EvaN@example.com', password: 'password', name: 'Evan Tedesco', max_guests: 1, admin: true)
+
+      expect(valid_user).to be_valid
+      expect(invalid_user).to be_invalid
+    end
+  end
 end
