a27254a8ecf4b4db19470a8fac90659f6b3b1988
Evan Tedesco <Evantedesco@gmail.com>
2014-07-09 20:43:43 -0600

    Refactored to eliminate duplication

--- spec/models/rsvp_spec.rb
+++ spec/models/rsvp_spec.rb
@@ -54,46 +54,46 @@ describe "Rsvp" do
       end
     end
 
+    context "Attending formatting" do 
+    let(:user) {create_user}
+
     it "should not allow wierd string value for attending" do
-      user = create_user
       rsvp = Rsvp.new(user: user, attending: "maybe")
       expect(rsvp).to be_invalid
     end
 
     it "should not allow nil value for attending" do
-      user = create_user
       rsvp = Rsvp.new(user: user, attending: nil)
       expect(rsvp).to be_invalid
     end
 
     it "should validate true value for attending" do
-      user = create_user
       rsvp = Rsvp.new(user: user, attending: "true", number_of_guests: 0)
       expect(rsvp).to be_valid
     end
 
     it "should validate false value for attending" do
-      user = create_user
       rsvp = Rsvp.new(user: user, attending: "false")
       expect(rsvp).to be_valid
     end
   end
+end
 
-  context "attending" do
-    let(:user) {create_user}
-    it "should return true when attending is true" do
-      rsvp = Rsvp.new(user: user, attending: "true")
-      expect(rsvp.attending).to be_true
-    end
-
-    it "should return false when attending is false" do
-      rsvp = Rsvp.new(user: user, attending: "false")
-      expect(rsvp.attending).to be_false
-    end
-
-    it "should return nil when attending is maybe" do
-      rsvp = Rsvp.new(user: user, attending: "maybe")
-      expect(rsvp.attending).to be_nil
-    end
+context "attending" do
+  let(:user) { create_user }
+  it "should return true when attending is true" do
+    rsvp = Rsvp.new(user: user, attending: "true")
+    expect(rsvp.attending).to be_true
   end
+
+  it "should return false when attending is false" do
+    rsvp = Rsvp.new(user: user, attending: "false")
+    expect(rsvp.attending).to be_false
+  end
+
+  it "should return nil when attending is maybe" do
+    rsvp = Rsvp.new(user: user, attending: "maybe")
+    expect(rsvp.attending).to be_nil
+  end
+end
 end
