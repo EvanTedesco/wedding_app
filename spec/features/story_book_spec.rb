63b0ac77395fb68789e5b2f6227c185551e454ce
Evan Tedesco <Evantedesco@gmail.com>
2014-07-16 23:19:49 -0600

    got rid of useless before blocks

--- spec/features/story_book_spec.rb
+++ spec/features/story_book_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 feature 'user visit the storybook index ' do
-  before do
-    @user = create_user
-  end
 
   scenario 'A logged in user can visit the storybook page' do
+    @user = create_user
+
     visit '/'
     click_link 'Login'
     fill_in 'user[email]', with: @user.email
     fill_in 'user[password]', with: @user.password
     click_button 'Login'
     click_link 'Our Story'
+
     expect(page).to have_content 'Our Storybook'
   end
 end
--- spec/features/wedding_party_spec.rb
+++ spec/features/wedding_party_spec.rb
@@ -1,16 +1,15 @@
 require 'spec_helper'
 feature 'user visit the wedding party index ' do
-  before do
-    @user = create_user
-  end
-
   scenario 'A logged in user can visit the Wedding Party page' do
+    @user = create_user
+
     visit '/'
     click_link 'Login'
     fill_in 'user[email]', with: @user.email
     fill_in 'user[password]', with: @user.password
     click_button 'Login'
     click_link 'Wedding Party'
+
     expect(page).to have_content 'Groomsmen'
     expect(page).to have_content 'Bridesmaids'
   end
