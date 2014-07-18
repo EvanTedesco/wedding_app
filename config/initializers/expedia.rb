ed55fc7fb5cb257228ba59bac4d8a377e03f0bc7
Evan Tedesco <Evantedesco@gmail.com>
2014-07-17 19:23:00 -0600

    added initializer for api call

---
+++ config/initializers/expedia.rb
@@ -0,0 +1,8 @@
+Expedia.setup do |config|
+	config.cid = 55505 # your cid once you go live.
+	config.api_key = ENV['EAN_KEY']
+	config.shared_secret = ENV['EAN_SHARED_SECRET']
+	config.locale = 'en_US' # For Example 'de_DE'. Default is 'en_US'
+	config.currency_code = 'USD' # For Example 'EUR'. Default is 'USD'
+	config.minor_rev = 26 # between 4-26 as of July 2014. Default is 4. 26 being latest
+end
--- spec/features/wedding_party_spec.rb
+++ spec/features/wedding_party_spec.rb
@@ -1,12 +1,12 @@
  require 'spec_helper'
 feature 'user visit the wedding party index ' do
   scenario 'A logged in user can visit the Wedding Party page' do
-    @user = create_user
+    user = create_user
 
     visit '/'
     click_link 'Login'
-    fill_in 'user[email]', with: @user.email
-    fill_in 'user[password]', with: @user.password
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
     click_button 'Login'
     click_link 'Wedding Party'
 
