3a7a101533a06bb9ef0cff660aafddcb9dcee9c6
Evan Tedesco <Evantedesco@gmail.com>
2014-08-04 21:51:44 -0600

    extracted method to get amenities

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,20 +6,25 @@ class Reservation
   def get_room_info
     info = {}
     @request["HotelRoomAvailabilityResponse"]["HotelRoomResponse"].each do |room|
-      amenities = room["RoomType"]["roomAmenities"]["RoomAmenity"].map do |amenity|
-        amenity["amenity"]
-      end
+
       info[room["rateDescription"]] = {
         average_rate: room["RateInfos"]["RateInfo"]["ChargeableRateInfo"]["@averageRate"],
         total_cost: room["RateInfos"]["RateInfo"]["ChargeableRateInfo"]["@total"],
         room_type_code: room["RoomType"]["@roomCode"],
         description: room["RoomType"]["descriptionLong"],
-        amenities: amenities,
+        amenities: get_amenities(room),
         smoking_preference: room["smokingPreferences"],
         rate_key: room["RateInfos"]["RateInfo"]["RoomGroup"]["Room"]["rateKey"],
         rate_code: room["rateCode"],
       }
     end
     info
+  end
+
+  def get_amenities(room)
+    amenities = room["RoomType"]["roomAmenities"]["RoomAmenity"].map do |amenity|
+      amenity["amenity"]
+    end
+    amenities
   end
 end
