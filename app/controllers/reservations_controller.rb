97306807e258a4212884c72fd71d0f4b2678aefc
Evan Tedesco <Evantedesco@gmail.com>
2014-08-05 08:29:02 -0600

    a user cannot check availability without logging in

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,6 @@
 class ReservationsController < ApplicationController
+  before_action :confirm_logged_in
+
   def new
   end
 
---
+++ spec/features/reservation_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+feature 'reservation manager' do
+  scenario 'a non-user cannot check availability' do
+    visit new_reservation_path
+
+    expect(page).to have_content 'You must be logged in to see this page'
+  end
+end
