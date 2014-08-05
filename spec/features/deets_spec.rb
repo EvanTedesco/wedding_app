27e388b9bb2375b922b0d2e408322a79ad9cb5ff
Evan Tedesco <Evantedesco@gmail.com>
2014-08-05 09:09:21 -0600

    wrote spec for reservations

--- spec/features/deets_spec.rb
+++ spec/features/deets_spec.rb
@@ -2,28 +2,17 @@ require 'spec_helper'
 
 feature 'The deets' do
   scenario 'The deets page is displayed when the link is clicked' do
-    new_time = '2014-07-11T21:53:58Z'
-    Timecop.freeze(new_time) do
-      VCR.use_cassette('features/deets') do
-        create_and_login_user
-        click_link 'The Deets'
-        click_link 'here'
+    create_and_login_user
+    click_link 'The Deets'
+    click_link 'here'
 
-        expect(page).to have_content 'Enter reservation details below'
-
-        fill_in 'arrival_date', with: '6/12/2015'
-        fill_in 'departure_date', with: '6/15/2015'
-        click_on 'Get pricing'
-
-        expect(page).to have_content 'Available rooms'
-      end
-    end
+    expect(page).to have_content 'Enter reservation details below'
   end
 
   scenario 'non-users cannot visit deets page' do
     visit '/'
     click_link 'The Deets'
 
-    expect(page).to have_content'You must be logged in to see this page'
+    expect(page).to have_content 'You must be logged in to see this page'
   end
 end
--- spec/features/reservation_spec.rb
+++ spec/features/reservation_spec.rb
@@ -9,4 +9,22 @@ feature 'reservation manager' do
 
     expect(page).to have_content 'You must be logged in to see this page'
   end
+
+  scenario 'a user checks availability' do
+    new_time = '2014-07-11T21:53:58Z'
+    Timecop.freeze(new_time) do
+      VCR.use_cassette('features/deets') do
+        create_and_login_user
+        visit new_reservation_path
+
+        fill_in 'arrival_date', with: '6/12/2015'
+        fill_in 'departure_date', with: '6/15/2015'
+        fill_in 'number_of_rooms', with: 1
+        fill_in 'number_of_adults', with: 2
+        click_on 'Get pricing'
+
+        expect(page).to have_content 'Available rooms'
+      end
+    end
+  end
 end
