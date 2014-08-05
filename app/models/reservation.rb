ef7a012c8b942883e809250a66269c96023f02b2
Evan Tedesco <Evantedesco@gmail.com>
2014-08-04 21:58:43 -0600

    extracted classes from reservation class

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,13 +9,13 @@ class Reservation
 
       info[room["rateDescription"]] = {
         average_rate: get_average_rate(room),
-        total_cost: room["RateInfos"]["RateInfo"]["ChargeableRateInfo"]["@total"],
-        room_type_code: room["RoomType"]["@roomCode"],
-        description: room["RoomType"]["descriptionLong"],
+        total_cost: get_total_cost(room),
+        room_type_code: get_room_type_code(room),
+        description: get_description(room),
         amenities: get_amenities(room),
-        smoking_preference: room["smokingPreferences"],
-        rate_key: room["RateInfos"]["RateInfo"]["RoomGroup"]["Room"]["rateKey"],
-        rate_code: room["rateCode"],
+        smoking_preference: get_smoking_preference(room) ,
+        rate_key: get_rate_key(room),
+        rate_code: get_rate_code(room),
       }
     end
     info
@@ -30,5 +30,29 @@ class Reservation
 
   def get_average_rate(room)
     room["RateInfos"]["RateInfo"]["ChargeableRateInfo"]["@averageRate"]
+  end
+
+  def get_total_cost(room)
+    room["RateInfos"]["RateInfo"]["ChargeableRateInfo"]["@total"]
+  end
+
+  def get_room_type_code(room)
+    room["RoomType"]["@roomCode"]
+  end
+
+  def get_description(room)
+    room["RoomType"]["descriptionLong"]
+  end
+
+  def get_smoking_preference(room)
+    room["smokingPreferences"]
+  end
+
+  def get_rate_key(room)
+    room["RateInfos"]["RateInfo"]["RoomGroup"]["Room"]["rateKey"]
+  end
+
+  def get_rate_code(room)
+    room["rateCode"]
   end
 end
